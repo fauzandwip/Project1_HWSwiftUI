cb4079fe6c47e857c47f518a9e5b6461aaa92266
fauzandp <fauzan27dp@gmail.com>
2023-05-29 17:48:42 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Fauzan Dwi Prasetyo on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
