e7e04e1bee2afcbac2b00bc5fc6f1ff6b9cddafe
fauzandp <fauzan27dp@gmail.com>
2023-05-30 13:24:03 +0700

    Project1_SwiftUI | WeSplit App add Picker with ForEach add NavigationStack

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -15,15 +15,25 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
-            
-            Section {
-                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-            }
+            .navigationTitle("WeSplit")
         }
     }
 }
